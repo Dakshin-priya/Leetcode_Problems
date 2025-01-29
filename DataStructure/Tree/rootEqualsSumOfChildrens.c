bc4ec54e1a89d6c33054cffefb1490e0cfb67c6d
Dakshin-priya <tsdakshinpriya@gmail.com>
2025-01-29 14:26:51 +0000

    root equals sum of childrens

---
+++ DataStructure/Tree/rootEqualsSumOfChildrens.c
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        int sum=root->left->val+root->right->val;
+        if(root->val==sum)
+            return true;
+        else
+            return false;
+    }
+};
