6e097288fdc2881091e6390d38e70d660a114c55
Dakshin Priya T S <tsdakshinpriya@gmail.com>
2025-02-25 11:32:58 +0530

    Create InvertBinaryTree.cpp

---
+++ DataStructure/Tree/InvertBinaryTree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)return NULL;
+        if(root->right==NULL&&root->left==NULL)return root;
+        
+        TreeNode*temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
