04895e5266d42d6fb24a780c207b59d4e52008f7
Dakshin Priya T S <tsdakshinpriya@gmail.com>
2025-04-06 07:24:26 +0530

    Create 11.java

---
+++ DataStructure/TwoPointers/11.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int max = 0;
+
+        while (i < j) {
+            int b = Math.min(height[i], height[j]); 
+            int l = j - i;
+            max = Math.max(max, b * l); 
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--; 
+            }
+        }
+
+        return max;
+    }
+}
